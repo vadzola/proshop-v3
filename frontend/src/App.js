d7724ca77e692599b25133693334c22f455534c0
Geb Ruiker <gebruiker@lenovo.kpn>
2023-07-11 16:12:47 +0200

    HomeScreen Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
     <>
-    <h1>App for ProShop</h1>
-    <p>Ovo je prvi app.</p>
-
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
     </>
-
-  )
-}
-export default App
+  );
+};
+export default App;
